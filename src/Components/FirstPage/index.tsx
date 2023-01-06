63cae0ed75b0a2f1ea1fdf52defb2e2eb200ccdd
faishal123 <faisal@tantowi.com>
2023-01-06 19:39:54 +0700

    initial commit

---
+++ src/Components/FirstPage/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+// import cover from "../../Images/cover.jpeg";
+import css from "./FirstPage.module.scss";
+
+const FirstPage = () => {
+  return (
+    <div className={css.container}>
+      <div className={css.imageContainer}>
+        <div className="basic-text-shadow-3 font-bodoni-moda font-18 margin--xlarge-b font-base-white font-letter-spacing-3">
+          THE WEDDING OF
+        </div>
+        <div className={`font-great-vibes ${css.textContainer}`}>
+          <div className="font-70 margin--medium-b">Vidya</div>
+          <div className="font-40 margin--medium-b">&</div>
+          <div className="font-70">Faishal</div>
+        </div>
+      </div>
+      <div className={css.greyBackground1}></div>
+      <div className={css.whiteBackground}></div>
+      <div className={css.greyBackground2}></div>
+    </div>
+  );
+};
+
+export default FirstPage;
---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MobileWrapper from "../../Components/MobileWrapper";
+import FirstPage from "../../Components/FirstPage";
+
+const Home = () => {
+  return (
+    <MobileWrapper>
+      <FirstPage />
+    </MobileWrapper>
+  );
+};
+
+export default Home;
